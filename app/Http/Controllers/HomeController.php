2b22ee8302d5df7125ff791c7c5e7423f3b5428c
Gebrina Divva Meuthia Zulma <gebgebdmz@gmail.com>
2020-08-10 01:45:03 +0700

    Myprofile and home fix for now, revised template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
+use Illuminate\Support\Facades\Route;
+use App\ActivityLog;
 
 class HomeController extends Controller
 {
@@ -11,18 +15,46 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function display(Request $request)
     {
+
+        $routes =  preg_match('/([a-z]*)@([a-z]*)/i', Route::currentRouteAction(), $matches);
+        $routes = $matches[0];
+        $action = $matches[2];
+    
+            DB::beginTransaction();
+            try {
+    
+         $as=   ActivityLog::create([
+    
+                'inserted_date'=>Carbon::now()->TimeZone('asia/jakarta'),
+                'username'=>"-",
+                'application'=>$routes,
+                'creator'=>"System",
+                'ip_user' => $request->ip(),
+                'action' => $action,
+                'description'=>"Display Home",
+                'user_agent' => $request->server('HTTP_USER_AGENT')
+            ]);
+
+            // dd($as);
+    
+            DB::commit();
+        } catch (\Exception $ex) {
+            DB::rollback();
+            return response()->json(['error' => $ex->getMessage()], 500);
+        }
         return view('home');
+        // return view('layouts.frontend');
     }
 }
