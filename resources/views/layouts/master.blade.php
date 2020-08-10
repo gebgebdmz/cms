34afda59df3fce4686848c1e597c3478a45c9b03
Gebrina Divva Meuthia Zulma <gebgebdmz@gmail.com>
2020-08-10 11:31:16 +0700

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -68,11 +68,6 @@
                     
                     @yield('content')
                     
-                    <select class="selectpicker" data-live-search="true">
-                        <option data-tokens="ketchup mustard">Hot Dog, Fries and a Soda</option>
-                        <option data-tokens="mustard">Burger, Shake and a Smile</option>
-                        <option data-tokens="frosting">Sugar, Spice and all things nice</option>
-                      </select>
                 </main>
                 <footer class="py-4 bg-light mt-5">
                     <div class="container-fluid">
